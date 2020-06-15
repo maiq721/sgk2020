bf642e338ea0bb32a8698417933680a61e4be3ed
vietcuong <vietcuong.uet@gmail.com>
2020-06-16 00:15:40 +0700

    thêm model trả về, viết api đăng ký

---
+++ BackEnd/ELearningAPI/ELearningAPI/TMSAPI/Controllers/ServiceResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TMSAPI.Controllers
+{
+    public class ServiceResponse
+    {
+        public const string DEFAULT_ERRORMESSAGE = "Có lỗi trong quá trình xử lý";
+
+        /// <summary>
+        /// Kết quả thực thi (true = success, false = lỗi)
+        /// </summary>
+        public bool Success { get; set; } = true;
+
+        /// <summary>
+        /// Mã lỗi phụ (để phân biệt chi tiết hơn các trường hợp lỗi ở mã lỗi chính)
+        /// </summary>
+        public int SubCode { get; set; }
+
+        /// <summary>
+        /// Nội dung lỗi hiển thị cho người dùng
+        /// </summary>
+        public string UserMessage { get; set; }
+
+        /// <summary>
+        /// Nội dung lỗi của hệ thống (phục vụ cho quá trình kiểm tra lỗi)
+        /// </summary>
+        public string SystemMessage { get; set; }
+
+        /// <summary>
+        /// Dữ liệu trả về
+        /// </summary>
+        public object Data { get; set; }
+
+        /// <summary>
+        /// Giờ hiện tại của server
+        /// </summary>
+        public DateTime ServerTime { get; set; } = DateTime.Now;
+    }
+}
--- BackEnd/ELearningAPI/ELearningAPI/TMSAPI/Controllers/UserController.cs
+++ BackEnd/ELearningAPI/ELearningAPI/TMSAPI/Controllers/UserController.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using TMSBL;
+using TMSBO.Model;
 
 namespace TMSAPI.Controllers
 {
-    public class UserController : ApiController
+    [RoutePrefix("api/user")]
+    public class UserController : BaseController
     {
+        public UserController() { }
+        public UserController(IUserBL IBL)
+        {
+            this.BL = IBL;
+            this.CurrentModelType = typeof(User);
+        }
+        [HttpPost]
+        [Route("SignUp")]
+        public async Task<ServiceResponse> SignUp([FromBody] User user)
+        {
+            var res = new ServiceResponse();
+            try
+            {
+                var objUser = BL.SaveData(user);
+                res.Data = objUser;
+            }
+            catch (Exception ex)
+            {
+                CommonLog.CommonErrorLog(ex, ex.Message);
+            }
+            return res;
+        }
+
     }
 }
