18fd99549fd197824a335d5ca35d05685c292d9f
nvm98 <manh98nv@gmail.com>
2020-06-15 22:13:23 +0700

    Thêm controller

--- BackEnd/ELearningAPI/ELearningAPI/TMSAPI/Controllers/UserController.cs
+++ BackEnd/ELearningAPI/ELearningAPI/TMSAPI/Controllers/UserController.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TMSBL;
 
 namespace TMSAPI.Controllers
 {
-    public class UserController : ApiController
+    [RoutePrefix("api/test")]
+    public class UserController : BaseController
     {
+        public UserController(IUserBL userBL)
+        {
+            this.BL = userBL;
+            //this.CurrentModelType = typeof(Model.Candidate);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public string Test()
+        {
+            return "Hello grand master, API is running!";
+        }
     }
 }
